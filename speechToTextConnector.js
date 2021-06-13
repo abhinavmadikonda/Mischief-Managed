fcfa8acc249df7a96243cc0efc64f2270b0a7ef8
amadikon <amadikon@cisco.com>
2021-06-13 23:53:55 +0530

    Alohomora and lumus maxima working

---
+++ speechToTextConnector.js
@@ -0,0 +1,93 @@
+var musicId = document.getElementById("homeScreenMusic");
+var containerId = document.getElementsByClassName("background")[0];
+var outputId = document.getElementById("output");
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var openSpells = ['alohomora', 'open', 'lumos maxima'];
+var grammar =  '#JSGF V1.0; grammar openSpells; public <openSpell> = ' + openSpells.join(' | ') + ' ;'
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+recognition.onresult = function(event){
+    var spell = event.results[0][0].transcript;
+    console.log(`abhi: ${JSON.stringify(event)} and ${spell}`);
+    nextScreen(spell);
+
+}
+
+recognition.onspeechend = function(event){
+    recognition.stop();
+}
+
+recognition.onnomatch = function(event){
+    outputId.innerHTML = "no input";
+}
+
+recognition.onerror = function(event){
+    outputId.textContent = "error occured!";
+}
+
+function startRecognition(){
+    if(checkConnectivity()){
+        musicId.pause();
+        musicOn = false;
+        recognition.start();
+        console.log('Ready to receive spell..');
+    } else {
+        alert("You are not connected to internet!")
+    }
+    
+}
+
+
+
+
+
+
+
+
+var currentScreen = 'door';
+var musicOn = true;
+function muteMusic(event) {
+    if(event.code == 'KeyM'){
+        if(musicOn) {
+            musicId.pause();
+            musicOn = false;
+        } else {
+            musicId.play();
+            musicOn = true;
+        }
+    }
+}
+
+document.addEventListener('keypress', muteMusic);
+
+function checkConnectivity(){
+    return navigator.onLine;
+}
+
+function nextScreen(spell) { 
+    switch(spell.toString().toLowerCase()){
+        case 'alohomora':
+        case 'open': {
+            containerId.style.backgroundImage ="url('resources/images/light.jpg')";
+            containerId.style.filter= "brightness(25%)";
+            break;
+            }
+        case 'lumos maxima': {
+            containerId.style.filter= "brightness(120%)";
+            break;
+        }
+    } 
+    outputId.innerHTML = spell;
+}
